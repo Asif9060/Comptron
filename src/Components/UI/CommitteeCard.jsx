6c74d1c59c4f9ab5b588e7ee39e6e0a8e5c8f6a5
Asif9060 <153944166+Asif9060@users.noreply.github.com>
2025-06-01 20:24:59 +0600

    .

--- src/Components/UI/CommitteeCard.jsx
+++ src/Components/UI/CommitteeCard.jsx
@@ -5,99 +5,94 @@ import logo from "../../assets/images/Comptron Logo.png";
 import { Link } from "react-router-dom";
 
 const Card = ({ imgSrc, alt, role, name, facebook, customId }) => {
-  const handleTouch = (e) => {
-    const article = e.currentTarget;
-    article.classList.add('show-overlay');
-    
-    // Remove the class after animation
-    setTimeout(() => {
-      article.classList.remove('show-overlay');
-    }, 3000); // Remove after 3 seconds
-  };
+   const handleTouch = (e) => {
+      const article = e.currentTarget;
+      article.classList.add("show-overlay");
 
-  return (
-    <article className="card__article group" onTouchStart={handleTouch}>
-      <div className="relative">
-        <img src={imgSrc} alt={alt} className="card__img" />
-        <div className="card__data">
-          <h2 className="card__title">{name}</h2>
-          <p className="text-xs text-white">{customId}</p>
-          <span className="card__description">{role}</span>
-        </div>
-        <div className="absolute inset-0 backdrop-blur-sm bg-black/30 transition-all duration-300 flex items-center justify-center opacity-0 md:group-hover:opacity-100 group-[.show-overlay]:opacity-100">
-          <Link
-            to={`/members/CommitteeProfile/${customId}`}
-            className="bg-blue-600 text-white py-2 px-4 rounded-md text-sm hover:bg-blue-700 transition transform scale-0 md:group-hover:scale-100 group-[.show-overlay]:scale-100 duration-300"
-          >
-            View Profile
-          </Link>
-        </div>
-      </div>
-    </article>
-  );
+      // Remove the class after animation
+      setTimeout(() => {
+         article.classList.remove("show-overlay");
+      }, 2000); // Remove after 3 seconds
+   };
+
+   return (
+      <article className="card__article group" onTouchStart={handleTouch}>
+         <div className="relative">
+            <img src={imgSrc} alt={alt} className="card__img" />
+            <div className="card__data">
+               <h2 className="card__title">{name}</h2>
+               <p className="text-xs text-white">{customId}</p>
+               <span className="card__description">{role}</span>
+            </div>
+            <div className="absolute inset-0 backdrop-blur-sm bg-black/30 transition-all duration-300 flex items-center justify-center opacity-0 md:group-hover:opacity-100 group-[.show-overlay]:opacity-100">
+               <Link
+                  to={`/members/CommitteeProfile/${customId}`}
+                  className="bg-blue-600 text-white py-2 px-4 rounded-md text-sm hover:bg-blue-700 transition transform scale-0 md:group-hover:scale-100 group-[.show-overlay]:scale-100 duration-300">
+                  View Profile
+               </Link>
+            </div>
+         </div>
+      </article>
+   );
 };
 
 // Main Component
 const CommiteeCard = () => {
-  
+   const [members, setMembers] = useState([]);
+   const [loading, setLoading] = useState(true);
 
-  const [members, setMembers] = useState([]);
-  const [loading, setLoading] = useState(true);
+   const fetchMembers = async () => {
+      try {
+         const response = await fetch(`${import.meta.env.VITE_API_URL}`); // Your backend URL
+         const data = await response.json();
+         // console.log("Fetched members data:", data);
+         setMembers(data); // Set members data into state
+      } catch (error) {
+         console.error("Error fetching members:", error);
+      } finally {
+         setLoading(false);
+      }
+   };
 
-  const fetchMembers = async () => {
-    try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}`); // Your backend URL
-      const data = await response.json();
-      // console.log("Fetched members data:", data);
-      setMembers(data); // Set members data into state
-    } catch (error) {
-      console.error("Error fetching members:", error);
-    }
-    finally{
-      setLoading(false)
-    }
-  };
+   useEffect(() => {
+      fetchMembers();
+   }, []);
 
-  useEffect(() => {
-    fetchMembers();
-  }, []);
-
-  if (loading) {
+   if (loading) {
       return (
-        <div className="flex justify-center items-center min-h-screen">
-          
-          <div className="loader-container">
-            <div className="rotating-circle"></div>
-            <img src={logo} alt="Comptron Logo" className="logo1" />
-          </div>
-        </div>
+         <div className="flex justify-center items-center min-h-screen">
+            <div className="loader-container">
+               <div className="rotating-circle"></div>
+               <img src={logo} alt="Comptron Logo" className="logo1" />
+            </div>
+         </div>
       );
-    }
+   }
 
-  return (
-    <div className="container md:translate-x-[14.5rem]">
-      <div className="card__container">
-        {members.length > 0 ? (
-          members.map((member) => (
-            <Card
-              key={member._id}
-              imgSrc={member.image}
-              // imgSrc={`${import.meta.env.VITE_API_URL.replace(
-              //   "/api/members",
-              //   ""
-              // )}${member.image}`}
-              alt={member.name}
-              role={member.role}
-              name={member.name}
-              facebook={member.socials.linkedin}
-              customId={member.customId}
-            />
-          ))
-        ) : (
-          <p>No members found</p>
-        )}
+   return (
+      <div className="container md:translate-x-[14.5rem]">
+         <div className="card__container">
+            {members.length > 0 ? (
+               members.map((member) => (
+                  <Card
+                     key={member._id}
+                     imgSrc={member.image}
+                     // imgSrc={`${import.meta.env.VITE_API_URL.replace(
+                     //   "/api/members",
+                     //   ""
+                     // )}${member.image}`}
+                     alt={member.name}
+                     role={member.role}
+                     name={member.name}
+                     facebook={member.socials.linkedin}
+                     customId={member.customId}
+                  />
+               ))
+            ) : (
+               <p>No members found</p>
+            )}
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 export default CommiteeCard;
